070c60f76b5bd1d410e8b475cf305a567a452cd0
HyungTae Lim <tyler.lim@sk.com>
2015-07-14 21:39:30 +0900

    Complete 2nd Course

---
+++ 1st/display_text.py
@@ -0,0 +1,31 @@
+# printl
+print('Hickory Disckry Dock! The mouse ran up the clock')
+print("Hickory Disckry Dock! The mouse ran up the clock")
+
+#print demo
+print('The capybars is the worlds largest rodent')
+
+#print ' / "
+print("It's beautiful day in the neighborhood")
+print('It\'s beautiful day in the neighborhood')
+
+#print multi line
+print('Hickory Disckry Dock! \nThe mouse ran up the clock')
+
+#print triple quotes and tab
+print("""Hickory Disckry Dock!
+The mouse ran up the clock""")
+
+print("""Hickory Disckry Dock!
+    The mouse ran up the clock""")
+
+print("""Hickory Disckry Dock!
+\tThe mouse ran up the clock""")
+
+#print use both single and double
+print('here is a double quote "' + " here is a single quote '")
+print("or you can hust do this \" does that work")
+
+print("can I just print \ on the screen? ")
+
+print("But what if I want \\news")
